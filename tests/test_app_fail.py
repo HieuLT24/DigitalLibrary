a7a1e1bb326328d77af69265703d667c39244e58
HieuLT24 <duchieuphung5@gmail.com>
2025-09-05 07:28:10 +0700

    disable test

--- tests/test_app_fail.py
+++ tests/test_app_fail.py
@@ -1,3 +1,3 @@
-def test_index_contains_nonexistent_text(client):
-    response = client.get('/')
-    assert b'NO_SUCH_TEXT_12345' in response.data  # cố ý fail
+# def test_index_contains_nonexistent_text(client):
+#     response = client.get('/')
+#     assert b'NO_SUCH_TEXT_12345' in response.data  # cố ý fail
