56678450d7e505df5f64b3388dbb79832f36928b
HieuLT24 <duchieuphung5@gmail.com>
2025-09-07 00:32:20 +0700

    fix: add column book.image

--- DL/config.py
+++ DL/config.py
@@ -6,4 +6,16 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:%s@localhost/librarydb?charset=utf8mb4" % quote(
     "root")
+    
+
+    
+    # Cloudinary configuration
+    CLOUDINARY_CLOUD_NAME = 'dsfghzlat'
+    CLOUDINARY_API_KEY = '784677976657694'
+    CLOUDINARY_API_SECRET = 'Y573YM27ykBpFzzWs7AIq2RWOtY'
+    
+    # Upload configuration
+    MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+    UPLOAD_FOLDER = 'Home/Library'
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
 
